22c02b18b3c432259c18b18e8763cf2e37da181b
Avemalaika <emanuelangel728@gmail.com>
2025-06-19 11:52:27 +0300

    fix: Account file

--- src/components/ui/toast.d.ts
+++ src/components/ui/toast.d.ts
@@ -2,6 +2,6 @@ import * as React from "react"
 import { type VariantProps } from "class-variance-authority"
 
 import { toastVariants } from "@/components/ui/toast"
-
+ 
 export type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>
 export type ToastActionElement = React.ReactElement<typeof ToastAction> 
