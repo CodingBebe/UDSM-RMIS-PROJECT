62f9d1fd766b027ed408f239b3e7295c2c244a3f
Avemalaika <emanuelangel728@gmail.com>
2025-06-19 11:13:57 +0300

    fix: resolve merge conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from "@/contexts/UserContext";
-import { Toaster as Sonner } from "sonner";
 
 import Login from "./pages/auth/Login";
 
@@ -46,7 +45,6 @@ const App = () => (
     <TooltipProvider delayDuration={0}>
       <UserProvider>
         <Toaster />
-        <Sonner />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
